dbd580737d5f29a31143ae0f454df4758c075d39
Francesco Pagano <fra.pgn@protonmail.com>
2020-02-20 17:19:25 +0100

    noia level 1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 for (var i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz');
+        var div = document.createElement('DIV');
+        div.setAttribute('class', 'item');
+        div.innerHTML = 'FizzBuzz';
+        document.getElementById('container').appendChild(div);
+        // console.log('FizzBuzz');
     } else if (i % 5 == 0) {
-        console.log('Buzz')
+        var div = document.createElement('DIV');
+        div.setAttribute('class', 'item');
+        div.innerHTML = 'Buzz';
+        document.getElementById('container').appendChild(div);
+        // console.log('Buzz')
     } else if (i % 3 == 0) {
-        console.log('Fizz')
+        var div = document.createElement('DIV');
+        div.setAttribute('class', 'item');
+        div.innerHTML = 'Fizz';
+        document.getElementById('container').appendChild(div);
+        // console.log('Fizz')
     } else {
-        console.log(i);
+        var div = document.createElement('DIV');
+        div.setAttribute('class', 'item');
+        div.innerHTML = i;
+        document.getElementById('container').appendChild(div);
+        // console.log(i);
     }
 }
